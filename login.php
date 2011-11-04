326fa03224ac7d712cdcdd422165831f5e3d14ea
Churchill Yik <476418125@qq.com>
2011-11-04 18:12:35 +0800

    make login page more clear

--- login.php
+++ login.php
@@ -1,29 +1,22 @@
 <?php
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                             TRAVIANX                                             //
-//            Only for advanced users, do not edit if you dont know what are you doing!             //
-//                                Made by: Dzoki & Dixie (TravianX)                                 //
-//                              - TravianX = Travian Clone Project -                                //
-//                                 DO NOT REMOVE COPYRIGHT NOTICE!                                  //
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 error_reporting(E_ALL);
 include("GameEngine/Account.php");
-if(isset($_GET['del_cookie'])) {
-	setcookie("COOKUSR","",time()-3600*24,"/");
+if(isset($_GET['del_cookie']))
+{
+	setcookie("COOKUSR", "", time() - 3600 * 24, "/");
 	header("Location: login.php");
 }
-if(!isset($_COOKIE['COOKUSR'])) {
+if(!isset($_COOKIE['COOKUSR']))
+{
 	$_COOKIE['COOKUSR'] = "";
 }
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
-	<head>
+<head>
 	<title><?php echo SERVER_NAME; ?></title>
-        <link REL="shortcut icon" HREF="favicon.ico"/>
+	<link REL="shortcut icon" HREF="favicon.ico"/>
 	<meta name="content-language" content="en" />
 	<meta http-equiv="cache-control" content="max-age=0" />
 	<meta http-equiv="imagetoolbar" content="no" />
@@ -35,78 +28,84 @@ if(!isset($_COOKIE['COOKUSR'])) {
    	<link href="gpack/travian_basic/lang/en/compact.css?f4b7c" rel="stylesheet" type="text/css" />
    	<link href="gpack/travian_basic/lang/en/lang.css?f4b7c" rel="stylesheet" type="text/css" />
 	<link href="<?php echo GP_LOCATE ?>/travian.css?f4b7c" rel="stylesheet" type="text/css" />
-    	<link href="<?php echo GP_LOCATE ?>/lang/en/lang.css" rel="stylesheet" type="text/css" />	
-       </head>
+	<link href="<?php echo GP_LOCATE ?>/lang/en/lang.css" rel="stylesheet" type="text/css" />	
+</head>
 
 <body class="v35 ie ie7" onload="initCounter()">
 
 <div class="wrapper">
-<div id="dynamic_header">
+	<div id="dynamic_header"></div>
+	<div id="header"></div>
+	<div id="mid">
+		<?php include("Templates/menu.tpl"); ?>
+		<div id="content" class="login">
+			<h1><img class="img_login" src="img/x.gif" alt="log in the game" /></h1>
+			<h5><img class="img_u04" src="img/x.gif" alt="login" /></h5>
+			<p><?php echo COOKIES; ?></p>
+			<form method="post" name="snd" action="login.php">
+				<input type="hidden" name="ft" value="a4" />
+				<table cellpadding="1" cellspacing="1" id="login_form">
+				<tbody>
+					<tr class="top">
+						<th><?php echo NAME; ?></th>
+						<td>
+							<input class="text" type="text" name="user" value="<?php echo $form->getDiff("user",$_COOKIE['COOKUSR']); ?>" maxlength="15" autocomplete='off' />
+							<span class="error"> <?php echo $form->getError("user"); ?></span>
+						</td>
+					</tr>
+					<tr class="btm">
+						<th><?php echo PASSWORD; ?></th>
+						<td>
+							<input class="text" type="password" name="pw" value="<?php echo $form->getValue("pw");?>" maxlength="20" autocomplete='off' /> 
+							<span class="error"><?php echo $form->getError("pw"); ?></span>
+						</td>
+					</tr>
+				</tbody>
+				</table>
+				<p class="btn">
+				<!--<input type="hidden" name="e1d9d0c" value="" />-->
+				<input type="image" value="login" name="s1"	onclick="xy();" id="btn_login" class="dynamic_img" src="img/x.gif" alt="login button" />
+				</p>
+			</form>
+		<?php
+		if ($form->getError("pw") == LOGIN_PW_ERROR)
+		{
+			echo "<p class=\"error_box\"><span class=\"error\">"
+			.PW_FORGOTTEN
+			."</span><br>"
+			.PW_REQUEST
+			."<br><a href=\"activate.php?npw=71699\">"
+			.PW_GENERATE
+			."</a></p>";
+		}
+		if($form->getError("activate") != "")
+		{
+			echo "<p class=\"error_box\"><span class=\"error\">"
+			.EMAIL_NOT_VERIFIED
+			."</span><br>"
+			.EMAIL_FOLLOW
+			."<br><a href=\"activate.php?usr="
+			.$form->getError("activate")
+			."\">"
+			.VERIFY_EMAIL
+			."</a></p>";
+		}
+		?>
+	</div>
+	<div id="side_info" class="outgame">
+		<?php
+		if(NEWSBOX1) { include("Templates/News/newsbox1.tpl"); }
+		if(NEWSBOX2) { include("Templates/News/newsbox2.tpl"); }
+		if(NEWSBOX3) { include("Templates/News/newsbox3.tpl"); }
+		?>
+	</div>
+	<div class="clear"></div>
 </div>
-<div id="header"></div>
-<div id="mid">
-<?php include("Templates/menu.tpl"); ?>
 
-<div id="content"  class="login">
-
-<h1><img class="img_login" src="img/x.gif" alt="log in the game" /></h1>
-<h5><img class="img_u04" src="img/x.gif" alt="login" /></h5>
-<p><?php echo COOKIES; ?></p>
-
-<form method="post" name="snd" action="login.php">
-<input type="hidden" name="ft" value="a4" />
-
-<table cellpadding="1" cellspacing="1" id="login_form">
-	<tbody>
-		<tr class="top">
-			<th><?php echo NAME; ?></th>
-			<td><input class="text" type="text" name="user" value="<?php echo $form->getDiff("user",$_COOKIE['COOKUSR']); ?>" maxlength="15" autocomplete='off' /> <span class="error"> <?php echo $form->getError("user"); ?></span></td>
-		</tr>
-		<tr class="btm">
-			<th><?php echo PASSWORD; ?></th>
-			<td><input class="text" type="password" name="pw" value="<?php echo $form->getValue("pw");?>" maxlength="20" autocomplete='off' /> <span class="error"><?php echo $form->getError("pw"); ?></span></td>
-		</tr>
-	</tbody>
-</table>
-
-<p class="btn">
-	<!--<input type="hidden" name="e1d9d0c" value="" />-->
-		<input type="image" value="login" name="s1"	onclick="xy();" id="btn_login" class="dynamic_img" src="img/x.gif" alt="login button"	/>
-</p>
-
-</form>
-<?php
-if ($form->getError("pw") == LOGIN_PW_ERROR) {
-echo "<p class=\"error_box\">
-	<span class=\"error\">".PW_FORGOTTEN."</span><br>
-	".PW_REQUEST."<br>
-	<a href=\"activate.php?npw=71699\">".PW_GENERATE."</a>
-</p>";
-}
-if($form->getError("activate") != "") {
-	echo "<p class=\"error_box\">
-	<span class=\"error\">".EMAIL_NOT_VERIFIED."</span><br>
-	".EMAIL_FOLLOW."<br>
-	<a href=\"activate.php?usr=".$form->getError("activate")."\">".VERIFY_EMAIL."</a>
-	</p>";
-}
-?>
-</div>
-<div id="side_info" class="outgame">
-<?php
-if(NEWSBOX1) { include("Templates/News/newsbox1.tpl"); }
-if(NEWSBOX2) { include("Templates/News/newsbox2.tpl"); }
-if(NEWSBOX3) { include("Templates/News/newsbox3.tpl"); }
-?>
-			</div>
-
+<div class="footer-stopper outgame"></div>
 <div class="clear"></div>
-			</div>
-
-			<div class="footer-stopper outgame"></div>
-            <div class="clear"></div>
-            
 <?php include("Templates/footer.tpl"); ?>
 <div id="ce"></div>
+
 </body>
 </html>
